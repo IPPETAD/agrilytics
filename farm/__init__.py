8d48ae2c631279ca8976dea2a47f88fab261ee39
Ranek Kiil <ranek.kiil@ualberta.ca>
2014-02-28 18:17:43 -0700

    Add database connection and make some fields.

--- farm/__init__.py
+++ farm/__init__.py
@@ -3,4 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['MONGO_URI'] = 'mongodb://farmspot:farmspot@troup.mongohq.com:10058/FarmSpot'
+
+
+
+
 import farm.views
--- farm/views.py
+++ farm/views.py
@@ -1,7 +1,22 @@
 from farm import app
 from flask import render_template, request
 
+from flask.ext.pymongo import PyMongo
+from bson.objectid import ObjectId
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
     return render_template('home.html')
+    
+@app.route('/fields')
+def fields():
+    fields = mongo.db.fields.find()
+    return render_template('fields.html', fields = fields)
+    
+@app.route('/field/<field_id>')
+def field(field_id):
+    field = mongo.db.fields.find_one({"_id": ObjectId(field_id) })
+    return render_template('field.html', field = field)
     
