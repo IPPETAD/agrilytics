e749db898e7719a2e85bf031c3b134df71d82264
Jacob Viau <jviau@ualberta.ca>
2014-03-01 00:10:46 -0700

    PORT PORT

--- runServer.py
+++ runServer.py
@@ -1,7 +1,8 @@
 from farm import app
+import os
 
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
