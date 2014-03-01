5c855f6833f0d55aadcc8093ab8674e0080ecbac
Ranek Kiil <ranek.kiil@ualberta.ca>
2014-02-28 18:52:10 -0700

    Bins.

--- farm/views.py
+++ farm/views.py
@@ -14,9 +14,19 @@ def index():
 def fields():
     fields = mongo.db.fields.find()
     return render_template('fields.html', fields = fields)
+	
+@app.route('/bins')
+def bins():
+    bins = mongo.db.bins.find()
+    return render_template('bins.html', bins = bins)
     
 @app.route('/field/<field_id>')
 def field(field_id):
     field = mongo.db.fields.find_one({"_id": ObjectId(field_id) })
     return render_template('field.html', field = field)
+    
+@app.route('/bin/<bin_id>')
+def bin(bin_id):
+    bin = mongo.db.bins.find_one({"_id": ObjectId(bin_id) })
+    return render_template('bin.html', bin = bin)
     
